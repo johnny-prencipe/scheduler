6061cf23cce3ce0124695af0f69a24f03c2312a7
Johnny Prencipe <johnny.prencipe@gmail.com>
2021-01-19 22:03:08 -0800

    First commit from day 1

---
+++ src/components/DayListItem.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "components/DayListItem.scss"
+import classnames from 'classnames';
+
+export default function DayListItem(props) {
+
+  const formatSpots = props => {
+    return (
+      props.spots === 0 ? 
+      `no spots remaining` :
+      props.spots === 1 ?
+      `${props.spots} spot remaining` :
+      `${props.spots} spots remaining`
+    )
+  };
+
+  const dayClass = classnames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+  
+  return (
+    <li onClick={() => props.setDay(props.name)} className={dayClass}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props)}</h3>
+    </li>
+  );
+}
