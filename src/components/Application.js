ee7d8762f2828f8446fabbda32f6af2a42cf618d
Johnny Prencipe <johnny.prencipe@gmail.com>
2021-01-22 02:25:06 -0800

    Refactor to use API instead of hardcoded date

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,83 +6,12 @@ import DayList from "components/DayList.js"
 import Appointment from "components/Appointment/index";
 import "components/Application.scss";
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "9am",
-    interview: {
-      student: "Alice Francisco",
-      interviewer: {
-        id: 2,
-        name: "Ryan Gosling",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 4,
-    time: "4pm",
-    interview: {
-      student: "Joseph Biden",
-      interviewer: {
-        id: 3,
-        name: "Mike Tyson",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "3pm",
-    interview: {
-      student: "Joseph Rogan",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  }
-];
-
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
-
 export default function Application(props) {
-  const [day, setDay] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
 
   useEffect(() => axios.get("/api/days"))
 
