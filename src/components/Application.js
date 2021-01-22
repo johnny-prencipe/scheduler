d030b2af046dea0a0974c1863924249666f1c582
Johnny Prencipe <johnny.prencipe@gmail.com>
2021-01-21 17:17:28 -0800

    Import stylesheet

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,64 @@
 import React, { useState } from "react";
 import DayList from "components/DayList.js"
 
+import "components/Appointment";
 import "components/Application.scss";
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "9am",
+    interview: {
+      student: "Alice Francisco",
+      interviewer: {
+        id: 2,
+        name: "Ryan Gosling",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "4pm",
+    interview: {
+      student: "Joseph Biden",
+      interviewer: {
+        id: 3,
+        name: "Mike Tyson",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "3pm",
+    interview: {
+      student: "Joseph Rogan",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  }
+];
+
 
 const days = [
   {
@@ -29,29 +86,29 @@ export default function Application(props) {
     <main className="layout">
       <section className="sidebar">
       <img
-  className="sidebar--centered"
-  src="images/logo.png"
-  alt="Interview Scheduler"
-/>
-<hr className="sidebar__separator sidebar--centered" />
+        className="sidebar--centered"
+        src="images/logo.png"
+        alt="Interview Scheduler"
+      />
+      <hr className="sidebar__separator sidebar--centered" />
 
-<nav className="sidebar__menu">
-  <DayList
-    days={days}
-    day={day}
-    setDay={setDay}
-  />
-</nav>
+      <nav className="sidebar__menu">
+        <DayList
+          days={days}
+          day={day}
+          setDay={setDay}
+        />
+      </nav>
 
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
-/>
+      <img
+        className="sidebar__lhl sidebar--centered"
+        src="images/lhl.png"
+        alt="Lighthouse Labs"
+      />
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
       </section>
     </main>
-  );
+      );
 }
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import InterviewerListItem from "components/InterviewerListItem";
+import "components/interviewerList.scss"
 
 export default function InterviewerList (props) {
   
