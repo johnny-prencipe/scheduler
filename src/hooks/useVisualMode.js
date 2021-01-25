e3e68180226efcaadbd5e949c252a937ed0479de
Johnny Prencipe <johnny.prencipe@gmail.com>
2021-01-24 17:32:21 -0800

    Write UseVisualMode

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [history, setHistory] = useState([initial]);
+
+  const transition = function (mode, replace = false) {
+    if (replace) {
+      setHistory(prev => [...prev.slice(0, -1), mode]);
+    } else {
+      setHistory(prev => [...prev, mode]);
+    }
+  };
+
+  const back = () => {
+    setHistory(prev => {
+      if (prev.length > 1) {
+        return prev.slice(0, -1);
+      }
+      return prev;
+    });
+  };
+
+  return { mode: history[history.length - 1], back, transition };
+}
