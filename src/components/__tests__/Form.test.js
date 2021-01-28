b0785e398067d98c919e5506864362ccb993a954
Johnny Prencipe <johnny.prencipe@gmail.com>
2021-01-28 15:24:17 -0800

    Finish Form tests

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -30,4 +30,48 @@ describe('Form', () => {
     expect(getByTestId("student-name-input")).toHaveValue("Lydia Miller-Jones");
   });
 
+  it("validates that the student name is not blank", () => {
+    /* 1. Create the mock onSave function */
+    const onSave = jest.fn();
+  
+    /* 2. Render the Form with interviewers and the onSave mock function passed as an onSave prop, the name prop should be blank or undefined */
+    const { getByText } = render(
+      <Form 
+      interviewers={interviewers}
+      onSave={onSave}
+      name=""
+      />
+      );
+      /* 3. Click the save button */
+      fireEvent.click(getByText("Save"));
+
+    expect(getByText(/Invalid params/i)).toBeInTheDocument();
+    expect(onSave).not.toHaveBeenCalled();
+  });
+
+it("calls onSave function when the name is defined", () => {
+  /* 1. Create the mock onSave function */
+  const onSave = jest.fn();
+  
+  /* 2. Render the Form with interviewers and the onSave mock function passed as an onSave prop, the name prop should be blank or undefined */
+  const { queryByText, getByText } = render(
+    <Form 
+    interviewers={interviewers}
+    interviewer="1"
+    name="Lydia Miller-Jones"
+    onSave={onSave}
+    />
+    );
+    /* 3. Click the save button */
+    fireEvent.click(getByText("Save"));
+  /* 3. validation is not shown */  
+  expect(queryByText(/Invalid params/i)).toBeNull();
+
+  /* 4. onSave is called once*/
+  expect(onSave).toHaveBeenCalledTimes(1);
+
+  /* 5. onSave is called with the correct arguments */
+  expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", "1");
+
+  });
 });
